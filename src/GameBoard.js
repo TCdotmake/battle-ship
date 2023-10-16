281cf72e846655a63d1dff3bb5436a7c36bf0bce
Tommy Chen <tommyjchen@hotmail.com>
2023-10-16 18:29:39 -0400

    getShipCoordinates implemented and tested for bound

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -11,8 +11,41 @@ function mkPosArr(size = 10) {
 }
 
 export default function GameBoard(size = 10) {
-  const gameBoard = {};
-  gameBoard.posArr = mkPosArr(size);
-  gameBoard.placeShip = (pos, shipLength, vertical = true) => {};
+  const gameBoard = {
+    occupied: [],
+    posArr: mkPosArr(size),
+    getShipCoordinates(pos, shipLength, vertical = true) {
+      let x, y;
+      const shipCoordinates = [];
+      [x, y] = pos.split(",");
+      if (vertical) {
+        for (let i = 0; i < shipLength; i++) {
+          let coordinate = `${x},${parseInt(y) + i}`;
+          if (this.posArr.includes(coordinate)) {
+            shipCoordinates.push(coordinate);
+          } else {
+            return false;
+          }
+        }
+      } else {
+        for (let i = 0; i < shipLength; i++) {
+          let coordinate = `${parseInt(x) + i},${y}`;
+          if (this.posArr.includes(coordinate)) {
+            shipCoordinates.push(coordinate);
+          } else {
+            return false;
+          }
+        }
+      }
+      for (let pos of shipCoordinates) {
+        if (this.occupied.includes(pos)) {
+          return false;
+        }
+      }
+      return shipCoordinates;
+    },
+    placeShip(pos, shipLength, vertical = true) {},
+  };
+
   return gameBoard;
 }
--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -15,3 +15,33 @@ test("GameBoard with size of zero should have a board.posArr that consist of []"
   const gameBoard = GameBoard(0);
   expect(gameBoard.posArr).toEqual([]);
 });
+
+test('No ship on board, check if getShipCoordinates("1,1", 1, false) return ["1,1"]', () => {
+  const gameBoard = GameBoard();
+  const result = gameBoard.getShipCoordinates("1,1,", 1, false);
+  expect(result).toEqual(["1,1"]);
+});
+
+test('No ship on board, check if getShipCoordinates("1,1", 2, false) return ["1,1", "2,1"]', () => {
+  const gameBoard = GameBoard();
+  const result = gameBoard.getShipCoordinates("1,1,", 2, false);
+  expect(result).toEqual(["1,1", "2,1"]);
+});
+
+test('No ship on board, check if getShipCoordinates("1,1", 2, true) return ["1,1", "1,2"]', () => {
+  const gameBoard = GameBoard();
+  const result = gameBoard.getShipCoordinates("1,1,", 2, true);
+  expect(result).toEqual(["1,1", "1,2"]);
+});
+
+test("It should be illegal to getShipCoordinates that result in out of bound, vertical", () => {
+  const gameBoard = GameBoard(3);
+  const result = gameBoard.getShipCoordinates("3,3", 2, true);
+  expect(result).toBe(false);
+});
+
+test("It should be illegal to getShipCoordinates that result in out of bound, horizontal", () => {
+  const gameBoard = GameBoard(3);
+  const result = gameBoard.getShipCoordinates("3,3", 2, false);
+  expect(result).toBe(false);
+});
