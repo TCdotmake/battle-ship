bacedbb53756c47e16410656bd0a9e988c85b1c5
Tommy Chen <tommyjchen@hotmail.com>
2023-10-16 17:22:08 -0400

    changed coordinate in posArr to numbers only

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,10 +1,9 @@
 function mkPosArr(size = 10) {
   const posArr = [];
   if (size > 0 && size <= 26) {
-    for (let i = 1; i <= size; i++) {
-      let n = String.fromCharCode(i + 64);
-      for (let j = 1; j <= size; j++) {
-        posArr.push(`${n},${j}`);
+    for (let x = 1; x <= size; x++) {
+      for (let y = 1; y <= size; y++) {
+        posArr.push(`${x},${y}`);
       }
     }
   }
@@ -14,6 +13,6 @@ function mkPosArr(size = 10) {
 export default function GameBoard(size = 10) {
   const gameBoard = {};
   gameBoard.posArr = mkPosArr(size);
-
+  gameBoard.placeShip = (pos, shipLength, vertical = true) => {};
   return gameBoard;
 }
--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -3,12 +3,12 @@ import GameBoard from "./GameBoard";
 
 test('GameBoard with size of one should have a board.posArr that consist of ["A1"]', () => {
   const gameBoard = GameBoard(1);
-  expect(gameBoard.posArr).toEqual([`A,1`]);
+  expect(gameBoard.posArr).toEqual([`1,1`]);
 });
 
 test('GameBoard with size of two should have a board.posArr that consist of ["A1","A2","B1",B2"]', () => {
   const gameBoard = GameBoard(2);
-  expect(gameBoard.posArr).toEqual(["A,1", "A,2", "B,1", "B,2"]);
+  expect(gameBoard.posArr).toEqual(["1,1", "1,2", "2,1", "2,2"]);
 });
 
 test("GameBoard with size of zero should have a board.posArr that consist of []", () => {
