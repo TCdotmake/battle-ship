e7f09dc7806cecb41a171e75b8764c4ab0b7e065
Tommy Chen <tommyjchen@hotmail.com>
2023-10-21 19:16:31 -0400

    able to place random ships on board, need testing

--- src/BattleShip.js
+++ src/BattleShip.js
@@ -1,5 +1,14 @@
 import BoardContainer from "./BoardContainer.js";
-
+import placeRandShips from "./placeRandShips.js";
+import Player from "./Player.js";
 export default function BattleShip() {
-  document.body.append(BoardContainer());
+  const p1Board = BoardContainer();
+  const p2Board = BoardContainer();
+  document.body.append(p1Board, p2Board);
+  const player1 = Player();
+  const player2 = Player();
+  player1.setOpponent(player2);
+  player2.setOpponent(player1);
+  placeRandShips(player1);
+  console.log(player1.board);
 }
