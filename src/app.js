c6dc69e4462a918fa73247ecf46d97a673d0f03c
Tommy Chen <tommyjchen@hotmail.com>
2023-10-28 10:36:27 -0400

    started implemented modal

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import NewGame from "./NewGame";
 import initStaticDOM from "./initStaticDOM";
 import mkGame from "./mkGame";
+import mkModal from "./modal/mkModal";
 
 export default function app() {
   initStaticDOM();
   const game = mkGame();
   addNewGameEL(game);
+
+  let appBG = document.getElementById("app-bg");
+  let dummyModal = mkModal();
+  appBG.append(dummyModal);
+  let aibtn = document.getElementById("toggle-ai-btn");
+  aibtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dummyModal.style.display = "block";
+  });
 }
 
 function addNewGameEL(gameObj) {
