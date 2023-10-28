95242782d1e97b00df857c7f615417395102ef0c
Tommy Chen <tommyjchen@hotmail.com>
2023-10-28 15:22:11 -0400

    turns are enforced by checking if player name match currentPlayer name

--- src/app.js
+++ src/app.js
@@ -10,9 +10,8 @@ export default function app() {
   initStaticDOM();
   const game = mkGame();
   addNewGameEL(game);
-  console.log(game);
-  cellsAddEL(game.p1);
-  cellsAddEL(game.p2);
+  cellsAddEL(game.p1, game);
+  cellsAddEL(game.p2, game);
   let appBG = document.getElementById("app-bg");
   appBG.append(switchModal);
 }
@@ -37,25 +36,32 @@ function openSwitchModal(gameObj) {
   modal.style.display = "block";
 }
 
-function cellsAddEL(player) {
+function cellsAddEL(player, gameObj) {
   let name = player.name;
   let id = `${name}-enemy`;
   const cells = document.getElementById(id).children;
   for (let index = 0; index < cells.length; index++) {
     cells[index].addEventListener("click", (e) => {
       e.preventDefault();
-      const x = e.target.dataset.x;
-      const y = e.target.dataset.y;
-      const valid = player.attack(x, y);
-      if (valid) {
-        updatePlayerBoard(player, x, y, e);
+      if (gameObj.active && gameObj.currentPlayer.name === player.name) {
+        const x = e.target.dataset.x;
+        const y = e.target.dataset.y;
+        const valid = player.attack(x, y);
+        if (valid) {
+          updateBoards(player, x, y, e);
+          //check for winner, make game inactive is won
+          checkForWinner(gameObj);
+          //switch currentPlayer
+          if (gameObj.active) {
+            gameObj.togglePlayer();
+          }
+        }
       }
     });
   }
 }
-function updatePlayerBoard(player, x, y, e) {
+function updateBoards(player, x, y, e) {
   const opponent = player.opponent;
-
   const token = opponent.getToken(x, y);
   const index = opponent.getIndex(x, y);
   const cell = document.getElementById(`${opponent.name}-self`).children[index];
@@ -68,5 +74,11 @@ function updatePlayerBoard(player, x, y, e) {
     e.target.append(mkIcon("missed", missedSVG));
     cell.innerHTML = "";
     cell.append(mkIcon("missed", missedSVG));
+  }
+}
+
+function checkForWinner(gameObj) {
+  if (gameObj.currentPlayer.opponent.isDefeated()) {
+    gameObj.active = false;
   }
 }
